f398cb2585b717021e973d3a0cb732b5297a7f83
boiledegg <tjrwns2163@naver.com>
2024-12-05 14:33:14 +0900

    add/#10: SideEffect 주석 추가

--- feature/addbook/src/main/java/com/sopt/anshim/addbook/type/AddBookSideEffect.kt
+++ feature/addbook/src/main/java/com/sopt/anshim/addbook/type/AddBookSideEffect.kt
@@ -1,5 +1,11 @@
 package com.sopt.anshim.addbook.type
 
+/**
+ * AddBookScreen의 사이드 이펙트
+ *
+ * [NavigateUp] 은 이전 화면으로 이동하는 효과
+ * [ShowToast] 는 토스트 메시지를 보여주는 효과
+ */
 sealed class AddBookSideEffect{
     data object NavigateUp: AddBookSideEffect()
     data class ShowToast(val message: String): AddBookSideEffect()
