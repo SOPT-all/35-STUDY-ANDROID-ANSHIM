3aa4de1aefaf288b4f4bbadd3bb7541169ed9f16
Thirfir <sangbbang31@gmail.com>
2024-12-05 16:42:05 +0900

    feat/#13 BookDataStore DI

---
+++ data/datastore/src/main/kotlin/com/sopt/di/DataSourceModule.kt
@@ -0,0 +1,28 @@
+package com.sopt.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.sopt.datastore.BookDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataSourceModule {
+
+    private val Context.bookDataStore: DataStore<Preferences> by preferencesDataStore(name = "book.pref")
+
+    @Singleton
+    @Provides
+    fun provideBookDataStore(
+        @ApplicationContext context: Context
+    ) : BookDataStore {
+        return BookDataStore(context.bookDataStore)
+    }
+}
